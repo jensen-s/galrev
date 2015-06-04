241acb6fe83623b6e352c5d4df63418a78739513
jensen-s <jscharnow@googlemail.com>
2015-06-04 20:56:11 +0200

    x implemented FileCrawler

--- src/main/java/org/jensen/galrev/crawl/FileCrawler.java
+++ src/main/java/org/jensen/galrev/crawl/FileCrawler.java
@@ -1,6 +1,8 @@
 package org.jensen.galrev.crawl;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jensen on 21.05.15.
@@ -8,6 +10,7 @@ import java.io.File;
 public class FileCrawler {
     private int updateFrequency;
     private ICrawlResultListener resultListener;
+    private List<CrawledEntity> resultPool = new ArrayList<>();
 
     public FileCrawler(ICrawlResultListener resultListener){
         this.resultListener = resultListener;
@@ -17,10 +20,36 @@ public class FileCrawler {
         this.updateFrequency = updateFrequency;
     }
 
+    /**
+     * Crawls for image files in the given directory
+     * @param startDirectory The starting directory. Must be - of course - a directory
+     */
     public void crawl(File startDirectory){
-
+        if (startDirectory.isDirectory()){
+            crawlDir(startDirectory);
+        }
+        if (!resultPool.isEmpty()){
+            resultListener.filesLocated(resultPool);
+        }
     }
 
+    private void crawlDir(File dir) {
+        for (File aFile: dir.listFiles()){
+            addResult(aFile);
+            if (aFile.isDirectory()){
+                crawlDir(aFile);
+            }
+        }
+    }
+
+    private void addResult(File aFile) {
+        CrawledEntity entity = new CrawledEntity(aFile);
+        resultPool.add(entity);
+        if (resultPool.size() == updateFrequency){
+            resultListener.filesLocated(resultPool);
+            resultPool.clear();
+        }
+    }
 
 
 }
